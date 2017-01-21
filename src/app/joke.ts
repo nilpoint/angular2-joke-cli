ca8acc16b3b9a36745a5f412aac6af6b89b70746
nilpoint <nilpoint@hotmail.com>
2017-01-21 19:47:57 +0800

    Display the punchline by clicking a button

--- src/app/joke.ts
+++ src/app/joke.ts
@@ -1,9 +1,15 @@
 export class Joke {
   setup: string;
   punchline: string;
+  hidden: boolean;
 
   constructor(setup: string, punchline: string){
     this.setup = setup;
     this.punchline = punchline;
+    this.hidden = true;
+  }
+
+  toggle(){
+    this.hidden = !this.hidden;
   }
 }
