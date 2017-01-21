aea9bd50ad7baeabdda3c40450e72e908d187e3f
nilpoint <nilpoint@hotmail.com>
2017-01-21 20:22:13 +0800

    Create a form to add a joke

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { JokeComponent } from './joke/joke.component';
 import { JokeListComponent } from './joke-list/joke-list.component';
+import { JokeFormComponent } from './joke-form/joke-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     JokeComponent,
-    JokeListComponent
+    JokeListComponent,
+    JokeFormComponent
   ],
   imports: [
     BrowserModule,
