ce7d797f1ab9655f40871510f581b322d544723d
nilpoint <nilpoint@hotmail.com>
2017-01-23 16:01:38 +0800

    Write shorter validation expressions

--- src/app/model-form/model-form.component.ts
+++ src/app/model-form/model-form.component.ts
@@ -9,25 +9,42 @@ import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angula
 export class ModelFormComponent implements OnInit {
 
   myform: FormGroup;
+  firstName: FormControl;
+  lastName: FormControl;
+  email: FormControl;
+  password: FormControl;
+  language: FormControl;
+
   langs: string[] = ["English", "French", "German"];
 
   constructor() { }
 
   ngOnInit() {
+    this.createFormControls();
+    this.createForm();
+  }
+
+  createFormControls(){
+    this.firstName = new FormControl('', Validators.required);
+    this.lastName = new FormControl('', Validators.required);
+    this.email = new FormControl('', [
+      Validators.required,
+      Validators.pattern("[^@]+@[^@]+")]);
+    this.password = new FormControl('',[
+      Validators.required,
+      Validators.minLength(8)]);
+    this.language = new FormControl();
+  }
+
+  createForm() {
     this.myform = new FormGroup({
       name: new FormGroup({
-        firstName: new FormControl('', Validators.required),
-        lastName: new FormControl('', Validators.required)
+        firstName: this.firstName,
+        lastName: this.lastName,
       }),
-      email: new FormControl('', [
-        Validators.required,
-        Validators.pattern("[^@]+@[^@]+")
-      ]),
-      password: new FormControl('',[
-        Validators.required,
-        Validators.minLength(8)
-      ]),
-      language: new FormControl()
+      email: this.email,
+      password: this.password,
+      language: this.language
     });
   }
 
