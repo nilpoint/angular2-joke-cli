b80d39778223c8837a0398e200559be46306d280
nilpoint <nilpoint@hotmail.com>
2017-01-23 15:25:53 +0800

    Add validation checks to the form via form model

--- src/app/model-form/model-form.component.ts
+++ src/app/model-form/model-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-model-form',
@@ -16,11 +16,17 @@ export class ModelFormComponent implements OnInit {
   ngOnInit() {
     this.myform = new FormGroup({
       name: new FormGroup({
-        firstName: new FormControl(),
-        lastName: new FormControl()
+        firstName: new FormControl('', Validators.required),
+        lastName: new FormControl('', Validators.required)
       }),
-      email: new FormControl(),
-      password: new FormControl(),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern("[^@]+@[^@]+")
+      ]),
+      password: new FormControl('',[
+        Validators.required,
+        Validators.minLength(8)
+      ]),
       language: new FormControl()
     });
   }
