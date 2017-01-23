4c08aadb81d27d65fabf42ca85a613a608ac7c4c
nilpoint <nilpoint@hotmail.com>
2017-01-23 15:10:49 +0800

    Link form model to form template
    Model driven forms 3.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -20,6 +20,7 @@ import { ModelFormComponent } from './model-form/model-form.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [],
--- src/app/model-form/model-form.component.ts
+++ src/app/model-form/model-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-model-form',
