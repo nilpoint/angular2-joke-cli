1a0835e9e067e4799e0d2e124a65af12cee4189c
nilpoint <nilpoint@hotmail.com>
2017-01-24 16:07:34 +0800

    Add Signup domain model

---
+++ src/app/signup.ts
@@ -0,0 +1,8 @@
+export class Signup {
+  constructor(
+    public firstName:string = '',
+    public lastName:string = '',
+    public email:string = '',
+    public password:string = '',
+    public language:string = '') {}
+}
