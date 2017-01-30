7b00a9c9011616d7fa36eacadebfda9531ab1927
John Smith <nilpoint@hotmail.com>
2017-01-30 11:09:56 +0800

    Create a search service

---
+++ src/app/search.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class SearchService {
+
+  apiRoot:string = 'https://itunes.apple.com/search';
+  results:Object[];
+  loading:boolean;
+  
+  constructor(private http:Http) {
+    this.results = [];
+    this.loading = false;
+  }
+
+  search(term:string){
+    let promise = new Promise((resolve, reject) => {
+      let apiURL = `${this.apiRoot}?term=${term}&media=music&limit=20`;
+      this.http.get(apiURL).toPromise().then(
+        res => { //Success
+          console.log(res.json());
+          resolve();
+        }
+      );
+    });
+    return promise;
+  }
+
+}
