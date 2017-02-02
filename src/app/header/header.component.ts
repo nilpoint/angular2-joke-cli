838894284c3f093eeb97e2af3371fa3ac36af9d3
John Smith <nilpoint@hotmail.com>
2017-02-02 13:21:08 +0800

    Navigate via a routerLink directive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,17 +7,9 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  goHome(){
-    this.router.navigate(['']);
-  }
-
-  goSearch(){
-    this.router.navigate(['search']);
   }
 
 }
