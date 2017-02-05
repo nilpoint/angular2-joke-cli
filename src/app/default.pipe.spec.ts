66bf209771ac498d42da016b66ad931f1c1e4413
John Smith <nilpoint@hotmail.com>
2017-02-05 09:56:04 +0800

    Add a test spec for pipe transform method

--- src/app/default.pipe.spec.ts
+++ src/app/default.pipe.spec.ts
@@ -1,5 +1,14 @@
 /* tslint:disable:no-unused-variable */
 import { DefaultPipe } from './default.pipe';
 
-describe('DefaultPipe', () => {
+describe('Pipe: Default', () => {
+  let pipe: DefaultPipe;
+
+  beforeEach(() => {
+    pipe = new DefaultPipe();
+  });
+
+  it('providing no value returns fallback', () => {
+    expect(pipe.transform('', 'http://image.home/200')).toBe('http://image.home/200');
+  });
 });
