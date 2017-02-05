bbd55eed971bc310f0325155c33c5f6003aa0432
John Smith <nilpoint@hotmail.com>
2017-02-05 09:36:23 +0800

    Add another test spec for isAuthenticated method
    This is the reverse case test.

--- src/app/user.service.spec.ts
+++ src/app/user.service.spec.ts
@@ -16,4 +16,9 @@ describe('Service: User', () => {
     localStorage.setItem('token', '1234');
     expect(service.isAuthenticated()).toBeTruthy();
   });
+
+  it('should return false from isAuthenticated when there is no token', () => {
+    // No token is set here and the token is already cleared in afterEach()
+    expect(service.isAuthenticated()).toBeFalsy();
+  });
 });
