bb13d9a147c58d6baace9c9dfbb176a781722058
John Smith <nilpoint@hotmail.com>
2017-02-05 09:13:04 +0800

    Set up and clean user service instance

--- src/app/user.service.spec.ts
+++ src/app/user.service.spec.ts
@@ -1,4 +1,14 @@
 import { UserService } from './user-service';
 
 describe('Service: User', () => {
+  let service: UserService;
+
+  beforeEach(() => {
+    service = new UserService();
+  });
+
+  afterEach(() => {
+    service = null;
+    localStorage.removeItem('token');
+  });
 });
