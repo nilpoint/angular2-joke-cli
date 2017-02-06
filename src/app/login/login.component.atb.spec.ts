70c57f3fe1d3a3627aa02457e96518a02d8efddb
nilpoint <nilpoint@hotmail.com>
2017-02-06 19:07:39 +0800

    Test state changes in the login component

--- src/app/login/login.component.atb.spec.ts
+++ src/app/login/login.component.atb.spec.ts
@@ -1,5 +1,7 @@
 /* tslint:disable:no-unused-variable */
 import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import { LoginComponent } from './login.component';
 import { UserService } from '../user-service';
@@ -8,6 +10,7 @@ describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
   let service: UserService;
+  let element: DebugElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -23,6 +26,9 @@ describe('LoginComponent', () => {
 
     // UserService provided to the TestBed
     service = TestBed.get(UserService);
+
+    // get the "a" element by CSS selector (e.g., by class name)
+    element = fixture.debugElement.query(By.css('a'));
   });
 
   afterEach(() => {    
@@ -40,5 +46,19 @@ describe('LoginComponent', () => {
     spyOn(service, 'isAuthenticated').and.returnValue(true);
     expect(component.needsLogin()).toBeFalsy();
     expect(service.isAuthenticated).toHaveBeenCalled();
+  });
+
+  it('login button is visible when the user is authenticated', () => {
+    // We initially expect the text inside the a tag to be blank.
+    // That’s because when Angular first loads no change detection has been 
+    // triggered and therefore the view doesn’t show either the Login or Logout text.
+    expect(element.nativeElement.textContent.trim()).toBe('');
+    // Trigger change detection
+    fixture.detectChanges();
+    expect(element.nativeElement.textContent.trim()).toBe('Login');
+    spyOn(service, 'isAuthenticated').and.returnValue(true);
+    expect(element.nativeElement.textContent.trim()).toBe('Login');
+    fixture.detectChanges();
+    expect(element.nativeElement.textContent.trim()).toBe('Logout');
   });
 });
